0eb8f90e87dd28abe9cadb8268073a60beab1d66
john@mac <zjsczjy04@hotmail.com>
2020-11-12 18:54:21 +0800

    first commit

---
+++ dist/index_props.js
@@ -0,0 +1,27 @@
+import projectConfig from '/pagic.config.js';
+export default {
+    config: { "root": "/", ...projectConfig },
+    'pagePath': "README.md",
+    'layoutPath': "_layout.tsx",
+    'outputPath': "index.html",
+    'title': undefined,
+    'content': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<p>Hello world</p>'
+        } }),
+    'head': null,
+    'script': React.createElement(React.Fragment, null,
+        React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
+        React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
+        React.createElement("script", { src: "/index.js", type: "module" })),
+    'contentTitle': undefined,
+    'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
+            __html: '<p>Hello world</p>'
+        } }),
+    'toc': null,
+    'author': undefined,
+    'contributors': [],
+    'date': "2020-11-12T10:24:16.076Z",
+    'updated': null,
+    'excerpt': "Hello world",
+    'cover': undefined
+};
